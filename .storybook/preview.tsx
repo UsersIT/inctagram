79d0af800e7bc38291b4f93ea40b388f4c00fca1
efValeron <masterbox06@gmail.com>
2024-03-27 16:43:12 +0500

    chore: sb files

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,15 +1,14 @@
 import type { Preview } from '@storybook/react'
-import '../src/app/styles/index.scss'
+import * as React from 'react'
 import { Inter } from 'next/font/google'
+
+import '../src/app/styles/index.scss'
 
 const inter = Inter({ subsets: ['latin', 'cyrillic'] })
 
 const preview: Preview = {
   parameters: {
-    backgrounds: {
-      default: 'dark',
-      values: [{ name: 'dark', value: '#0D0D0D' }],
-    },
+    backgrounds: { default: 'dark', values: [{ name: 'dark', value: '#0D0D0D' }] },
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
