649afd24b2ec1cc65208a2aec81f885eb539e338
Sergey Koshelev <shivacharya2019@gmail.com>
2024-04-09 15:57:11 +0300

    IN-21 refactor: add description

--- src/shared/ui/modal/modal.stories.tsx
+++ src/shared/ui/modal/modal.stories.tsx
@@ -6,7 +6,54 @@ import { Button } from '../button'
 import { Modal } from './modal'
 
 const meta = {
+  argTypes: {
+    onClose: {
+      description: 'Optional callback function that is called to close the modal.',
+    },
+    onNextButton: {
+      description: 'Optional callback function that is called when the next button is clicked.',
+    },
+    onPreviousButton: {
+      description: 'Optional callback function that is called when the previous button is clicked.',
+    },
+    open: {
+      control: '-',
+      description:
+        "The open prop is a boolean that determines the visibility of the Modal component. It requires an external state management within the parent component to toggle the modal's visibility. ",
+    },
+    showCloseButton: {
+      description: 'Optional callback function that is called to display the modal close button.',
+    },
+    showNextButton: {
+      description: 'Optional boolean that determines whether to show a next button in the modal.',
+    },
+    showPreviousButton: {
+      description: 'Optional boolean that determines whether to show a close button in the modal.',
+    },
+    size: {
+      control: { type: 'radio' },
+      description:
+        'Optional value defining the size of the modal:\n' +
+        '   * sm - 378px,\n' +
+        '   * md - 492px,\n' +
+        '   * lg - 644px.\n' +
+        '   * For other values use className',
+    },
+    title: {
+      control: { type: 'text' },
+      description: 'Optional string that sets the title of the modal.',
+    },
+  },
   component: Modal,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Modal component is based on Dialog from Radix UI and uses animations from Framer Motion.',
+      },
+    },
+  },
+
   tags: ['autodocs'],
   title: 'Components/Modal',
 } satisfies Meta<typeof Modal>
@@ -18,6 +65,10 @@ export const DefaultModal: Story = {
   args: {
     children: 'Modal',
     open: false,
+    showCloseButton: false,
+    showNextButton: false,
+    showPreviousButton: false,
+    size: 'md',
     title: 'Modal',
   },
   render: args => {
