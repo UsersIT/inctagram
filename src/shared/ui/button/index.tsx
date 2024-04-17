724325a50c5992213abb82671212759aa2f03307
Marina Diubailo <marinadiubailo@gmail.com>
2024-04-17 10:03:59 +0300

    refactor: add isLoading prop, fix props and ref types

--- src/shared/ui/button/index.tsx
+++ src/shared/ui/button/index.tsx
@@ -1,38 +1,75 @@
-import { ComponentPropsWithoutRef, ElementType, ForwardedRef, forwardRef } from 'react'
+/* eslint-disable react/display-name */
+import {
+  ComponentPropsWithRef,
+  ComponentPropsWithoutRef,
+  ElementType,
+  ReactNode,
+  forwardRef,
+} from 'react'
 
 import { clsx } from 'clsx'
 
 import s from './button.module.scss'
 
+import { Spinner } from '../spinner'
+
 type ButtonVariant = 'outlined' | 'primary' | 'secondary' | 'text'
 
-type InferType<T> = T extends ElementType<infer U> ? U : never
-
-export type ButtonProps<T extends ElementType = 'button'> = {
+type AsPolymorphProp<T extends ElementType> = {
   as?: T
-  fullWidth?: boolean
-  variant?: ButtonVariant
-} & ComponentPropsWithoutRef<T>
+}
 
-export const Button = forwardRef(
-  <T extends ElementType = 'button'>(props: ButtonProps<T>, ref: ForwardedRef<InferType<T>>) => {
+type ElementProps = {
+  fullWidth?: boolean
+  isLoading?: boolean
+  variant?: ButtonVariant
+}
+
+type PolymorphProps<T extends ElementType, ElementProps = {}> = AsPolymorphProp<T> &
+  ElementProps &
+  Omit<ComponentPropsWithoutRef<T>, keyof (ElementProps & AsPolymorphProp<T>)>
+
+type PolymorphRef<T extends ElementType> = ComponentPropsWithRef<T>['ref']
+
+type PolymorphPropRef<T extends ElementType> = { ref?: PolymorphRef<T> }
+
+type PolymorphPropsWithRef<T extends ElementType, ElementProps = {}> = PolymorphProps<
+  T,
+  ElementProps
+> &
+  PolymorphPropRef<T>
+
+type TagComponent = <T extends ElementType = 'button'>(
+  props: PolymorphPropsWithRef<T, ElementProps>
+) => ReactNode
+
+const ButtonPolymorph: TagComponent = forwardRef(
+  <T extends ElementType = 'button'>(props: PolymorphPropsWithRef<T>, ref?: PolymorphRef<T>) => {
     const {
-      as: Component = 'button',
+      as: Tag = 'button',
       children,
-      className,
-      fullWidth = false,
+      className = '',
+      disabled,
+      fullWidth,
+      isLoading,
       variant = 'primary',
       ...rest
     } = props
 
-    const classNames = clsx(s.button, s[variant], fullWidth && s.fullWidth, className)
+    const tagClassName = clsx(
+      s.button,
+      s[variant],
+      fullWidth && s.fullWidth,
+      className,
+      disabled && 'href' in rest && s.disabled
+    )
 
     return (
-      <Component className={classNames} ref={ref} {...rest}>
-        {children}
-      </Component>
+      <Tag className={tagClassName} disabled={disabled} ref={ref} {...rest}>
+        {isLoading ? <Spinner /> : children}
+      </Tag>
     )
   }
 )
 
-Button.displayName = 'Button'
+export const Button = ButtonPolymorph
