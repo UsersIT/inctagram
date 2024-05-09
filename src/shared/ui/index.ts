abd89580141382ff82b11be645b689b4429342b8
Marina Diubailo <marinadiubailo@gmail.com>
2024-05-09 12:14:00 +0300

    IN-30 fix: fix select styles and add modal to public api

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,6 +1,7 @@
 export * from './button'
 export * from './card'
 export * from './Checkbox'
+export * from './modal/modal'
 export * from './spinner'
 export * from './scrollArea'
 export * from './select'
