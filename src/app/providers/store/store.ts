1c2ec6a0e84dc7351340be3be36ac5a05a8d5f58
Marina Diubailo <marinadiubailo@gmail.com>
2024-05-10 11:35:48 +0300

    IN-30 feat: add store provider

---
+++ src/app/providers/store/store.ts
@@ -0,0 +1,27 @@
+import { useDispatch, useSelector, useStore } from 'react-redux'
+
+import { baseApi } from '@/src/shared/api/baseApi'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query/react'
+
+const rootReducer = combineReducers({
+  [baseApi.reducerPath]: baseApi.reducer,
+})
+
+export const makeStore = () => {
+  return configureStore({
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
+    reducer: rootReducer,
+  })
+}
+
+setupListeners(makeStore().dispatch)
+
+export type AppStore = ReturnType<typeof makeStore>
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
+
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
+export const useAppStore = useStore.withTypes<AppStore>()
