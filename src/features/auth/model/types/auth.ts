d7f31ed480570c86e3fff8fbf9f6b2d6fb3944f9
Marina Diubailo <marinadiubailo@gmail.com>
2024-05-18 16:01:34 +0300

    IN-15 feat: add authApi and its types

---
+++ src/features/auth/model/types/auth.ts
@@ -0,0 +1,11 @@
+export type RegisterInput = {
+  baseUrl?: string
+  email: string
+  password: string
+  userName: string
+}
+
+export type RegistrationEmailResendingInput = {
+  baseUrl?: string
+  email: string
+}
