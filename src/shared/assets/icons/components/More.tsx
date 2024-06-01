a76849a43fa3e486be50ab8395f85f0d38caaa88
Sergey Koshelev <shivacharya2019@gmail.com>
2024-06-01 21:59:52 +0300

    IN-20 feat: add More icon

---
+++ src/shared/assets/icons/components/More.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import { Ref, SVGProps, forwardRef, memo } from 'react'
+const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
+  <svg
+    fill={'currentColor'}
+    height={24}
+    ref={ref}
+    width={24}
+    xmlns={'http://www.w3.org/2000/svg'}
+    {...props}
+  >
+    <path
+      d={
+        'M12 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM19 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM5 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z'
+      }
+    />
+  </svg>
+)
+
+export const More = memo(forwardRef(SvgComponent))
