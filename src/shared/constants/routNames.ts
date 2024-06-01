e10996d34c492157246e43829d8847553f882af9
Sergey Koshelev <shivacharya2019@gmail.com>
2024-06-01 22:02:51 +0300

    IN-20 feat: add RouteNames variable

---
+++ src/shared/constants/routNames.ts
@@ -0,0 +1,10 @@
+export const RouteNames = {
+  AUTH: '/auth',
+  FAVORITES: '/favorites',
+  HOME: '/',
+  PROFILE: '/profile',
+  PROFILE_SETTINGS: '/settings',
+  SIGN_IN: '/auth/sign-in',
+  SIGN_UP: '/auth/sign-up',
+  STATISTICS: '/statistics',
+} as const
