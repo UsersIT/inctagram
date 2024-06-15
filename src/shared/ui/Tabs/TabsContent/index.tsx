e0abcf60dbca2a616f622ce57cecef2798065fa5
Valeriy <masterbox06@gmail.com>
2024-06-15 22:02:09 +0500

    chore: remove unnecessary style file

--- src/shared/ui/Tabs/TabsContent/index.tsx
+++ src/shared/ui/Tabs/TabsContent/index.tsx
@@ -1,16 +1,13 @@
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
 import * as TabsRadix from '@radix-ui/react-tabs'
-import clsx from 'clsx'
-
-import s from './TabsContent.module.scss'
 
 type Props = ComponentPropsWithoutRef<typeof TabsRadix.Content>
 
 export const TabsContent = forwardRef<ElementRef<typeof TabsRadix.Content>, Props>(
   ({ children, className, ...rest }, ref) => {
     return (
-      <TabsRadix.Content className={clsx(s.content, className)} ref={ref} {...rest}>
+      <TabsRadix.Content className={className} ref={ref} {...rest}>
         {children}
       </TabsRadix.Content>
     )
