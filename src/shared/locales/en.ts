5c9d1dfd5452eaac270a9f2a9a8bbd442130c89f
Marina Diubailo <marinadiubailo@gmail.com>
2024-07-09 17:11:09 +0300

    IN-39 refactor: change SignUp form validation and Combobox styles

--- src/shared/locales/en.ts
+++ src/shared/locales/en.ts
@@ -133,7 +133,8 @@ export const en: LocaleType = {
     maxLength: (len: number) => `Maximum ${len} characters`,
     minLength: (len: number) => `Minimum ${len} characters`,
     passwordConfirmation: 'The passwords must match',
-    passwordVerification: 'The password must contain ',
+    passwordVerification:
+      'The password must contain at least one uppercase letter, one lowercase letter, one number and one special character ',
     required: 'Required field',
     userNameVerification: 'Username should contain 0-9, a-z, A-Z, _, -',
     usernameExists: 'User with this username is already registered',
