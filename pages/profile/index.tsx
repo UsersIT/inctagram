9bc029b4c5a696811ebedbab2869ceffd9aefaa8
Halina2610 <vievamaxa@gmail.com>
2024-07-29 12:06:00 +0300

    fix

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -3,18 +3,8 @@ import type { NextPageWithLayout } from '@/src/shared/types/next'
 import type { ReactElement } from 'react'
 
 import PrivateLayout from '@/src/app/layouts/PrivateLayout/PrivateLayout'
-import { useGetProfileQuery } from '@/src/features/profile/api/profileApi'
-import { ProfilePhoto } from '@/src/features/profile/ui/ProfilePhoto/ProfilePhoto'
-
 const Page: NextPageWithLayout = () => {
-  const { data, refetch } = useGetProfileQuery(undefined)
-
-  return (
-    <>
-      <h1>Profile</h1>
-      <ProfilePhoto photoUrlFromServer={data?.avatars[0]?.url} refetch={refetch} />
-    </>
-  )
+  return <h1>Profile</h1>
 }
 
 Page.getLayout = function getLayout(page: ReactElement) {
