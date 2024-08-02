3400abb6123089fe5f08b81c0e819260033cec27
Yuri-dot-com <yuri.ivanov.mail@gmail.com>
2024-08-02 15:49:15 -0700

    add middleware for automatically internationalization by any pathname

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
-    locales: ["en", "ru"],
-    defaultLocale: "ru",
+    locales: ['default', 'en', 'ru'],
+    defaultLocale: 'default',
+    localeDetection: false
   },
   images: {
     domains: ['staging-it-incubator.s3.eu-central-1.amazonaws.com'],
