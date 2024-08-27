cfa6f21e627a1f004cec4870395a8b11b9ee7c1d
Halina2610 <vievamaxa@gmail.com>
2024-08-27 16:37:42 +0300

    add   onNextClick, onPrevClick

--- src/shared/ui/Carousel/Carousel.tsx
+++ src/shared/ui/Carousel/Carousel.tsx
@@ -1,23 +1,50 @@
+/* eslint-disable import/extensions */
+import { useEffect } from 'react'
+
 import { ArrowIosBackOutline, ArrowIosForward } from '@/src/shared/assets/icons'
 import clsx from 'clsx'
 import Image from 'next/image'
+import SwiperCore from 'swiper'
 import { Navigation, Pagination } from 'swiper/modules'
 import { Swiper, SwiperProps, SwiperSlide } from 'swiper/react'
 
-// eslint-disable-next-line import/extensions
 import 'swiper/css'
-// eslint-disable-next-line import/extensions
 import 'swiper/css/navigation'
-// eslint-disable-next-line import/extensions
 import 'swiper/css/pagination'
 
 import s from './Carousel.module.scss'
 
 export type Props = {
   imagesUrl?: { url: string }[]
+  onNextClick?: (index: number) => void
+  onPrevClick?: (index: number) => void
 } & Omit<SwiperProps, 'modules' | 'navigation' | 'pagination' | 'slidesPerView' | 'spaceBetween'>
 
-export const Carousel = ({ className, imagesUrl = [], ...props }: Props) => {
+export const Carousel = ({
+  className,
+  imagesUrl = [],
+  onNextClick,
+  onPrevClick,
+  ...props
+}: Props) => {
+  useEffect(() => {
+    const swiperEl = document.querySelector('.swiper-container') as HTMLElement & {
+      swiper: SwiperCore
+    }
+
+    if (swiperEl?.swiper) {
+      const swiperInstance = swiperEl.swiper
+
+      swiperInstance.on('slideChange', () => {
+        if (swiperInstance.activeIndex > swiperInstance.previousIndex && onNextClick) {
+          onNextClick(swiperInstance.activeIndex)
+        } else if (swiperInstance.activeIndex < swiperInstance.previousIndex && onPrevClick) {
+          onPrevClick(swiperInstance.activeIndex)
+        }
+      })
+    }
+  }, [onNextClick, onPrevClick])
+
   if (imagesUrl.length === 0) {
     return null
   }
