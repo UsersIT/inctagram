577bdd5fdfab251f724d281986812937a4cec045
Marina Diubailo <marinadiubailo@gmail.com>
2024-10-02 14:28:39 +0300

    IN-44 feat: add redirect from Profile Page if no auth data

--- src/pages/profile/ui/ProfilePage/ProfilePage.tsx
+++ src/pages/profile/ui/ProfilePage/ProfilePage.tsx
@@ -1,6 +1,10 @@
+import { useEffect } from 'react'
+
+import { useMeQuery } from '@/src/features/auth'
 import { PostCreator } from '@/src/features/createPost'
 import { PostsList } from '@/src/features/posts'
 import { ProfileInfo } from '@/src/features/profile'
+import { routes } from '@/src/shared/constants/routes'
 import { useRouter } from 'next/router'
 
 import s from './ProfilePage.module.scss'
@@ -8,6 +12,13 @@ import s from './ProfilePage.module.scss'
 export const ProfilePage = () => {
   const router = useRouter()
   const { modal } = router.query
+  const { data: isAuth, isLoading } = useMeQuery()
+
+  useEffect(() => {
+    if (!isLoading && !isAuth) {
+      router.push(routes.LOGIN)
+    }
+  }, [isAuth, isLoading, router])
 
   return (
     <main className={s.page}>
